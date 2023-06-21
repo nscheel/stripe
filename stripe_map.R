5eb225b5360311dadc78deeb3c1f745b11efd31b
nscheel <97459427+nscheel@users.noreply.github.com>
2023-06-21 15:16:46 -0500

    Add files via upload

---
+++ stripe_map.R
@@ -0,0 +1,33 @@
+#installing packages
+#install.packages(c("ggplot2", "devtools", "dplyr", "stringr"))
+#install.packages(c("maps", "mapdata"))
+#install.packages("ggmap")
+
+#loading packages
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
+
+# creating a sample data.frame with your lat/lon points
+lon <- c(-89.48,-89.35)
+lat <- c(43.05, 43.16)
+coordinates <- as.data.frame(cbind(lon,lat))
+
+#loading in site coordinates
+getwd()
+setwd("Desktop/stripe_data")
+sites <- read.csv("stripe_coordinates.csv")
+
+
+#getting the map
+register_google(key = "AIzaSyBVVexkM5Z51lP4uDzVjEnxwrCm_JMUriE")
+map_mendota <- get_map(location = c(lon = mean(coordinates$lon), lat = mean(coordinates$lat)), zoom = 12,
+                      maptype = "satellite", scale = 2)
+figure_1 <- ggmap(map_mendota)
+map_of_stripe_sites <- figure_1 + geom_point(data=sites, aes(x=lon, y=lat), color="red", size=1, alpha=0.5) 
+
+map_of_stripe_sites
+
+
+
